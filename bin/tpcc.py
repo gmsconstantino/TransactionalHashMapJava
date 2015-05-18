707ca8a358451ec1188d6a841688222edfc20ad8
Constantino Gomes <gmsconstantino@users.noreply.github.com>
2015-05-18 18:29:38 +0100

    correction tpcc script

--- bin/tpcc.py
+++ bin/tpcc.py
@@ -18,6 +18,6 @@ threads = [1,2,4,8,16,20]
 
 for thread in threads:
     for alg in  algorithm:
-        ycsb_command = ["java", "--Xmx20g -Xms15g", "-cp", "target/myhashdb-1.0.3.jar:../thrift-0.9.2/lib/java/build/libthrift-0.9.2.jar",  \
+        ycsb_command = ["java", "-Xmx20g -Xms15g", "-cp", "target/myhashdb-1.0.3.jar:../thrift-0.9.2/lib/java/build/libthrift-0.9.2.jar",  \
                         "bench.tpcc.TpccEmbeded", "-w", str(thread), "-c", str(thread), "-t", str(time), "-tp", alg, "-B" ]
         subprocess.call(ycsb_command)
