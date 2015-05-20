71045abf1dab25fae356388605995d691a3b5cf6
Constantino Gomes <gmsconstantino@users.noreply.github.com>
2015-05-20 14:39:51 +0100

    update heap java on micro script

--- bin/micro.py
+++ bin/micro.py
@@ -24,6 +24,6 @@ for thread in threads:
     for x in range(0,len(perc_read)):
         for ops in opsArr:
             for alg in  algorithm:
-                ycsb_command = ["java", "-Xmx20g", "-Xms2g", "-cp", "target/myhashdb-1.0.3.jar", \
+                ycsb_command = ["java", "-Xmx20g", "-Xms8g", "-cp", "target/myhashdb-1.0.3.jar", \
                                 "bench.MicroSI", alg ,str(time), str(thread),str(perc_read[x]) ,str(ops), str(perc_write[x]) ]
                 subprocess.call(ycsb_command)
