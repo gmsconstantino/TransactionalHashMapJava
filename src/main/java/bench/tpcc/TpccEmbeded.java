85e28502bb1271fccbe180d5680d1436ac4b64fe
Constantino Gomes <gmsconstantino@users.noreply.github.com>
2015-05-23 16:41:31 +0100

    remove scanner

--- src/main/java/bench/tpcc/TpccEmbeded.java
+++ src/main/java/bench/tpcc/TpccEmbeded.java
@@ -50,8 +50,8 @@ public class TpccEmbeded {
         System.out.println("Size database = "+size);
 
         // TODO: Remove scanner only debug
-        Scanner in = new Scanner(System.in);
-        in.nextLine();
+//        Scanner in = new Scanner(System.in);
+//        in.nextLine();
 
 //        measure time
         System.out.printf("\nSTART BENCHMARK.\n\n");
