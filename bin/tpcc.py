e0fd2a76b4b2d98fbf3b0c23191aed50d5389b30
Constantino Gomes <gmsconstantino@users.noreply.github.com>
2015-06-18 00:46:51 +0100

    Fix NullPointer, used wrong variable

--- bin/tpcc.py
+++ bin/tpcc.py
@@ -11,13 +11,13 @@ algorithm = [
     "NMSI"
 ]
 
-time = 30
+time = 60
 
 threads = [1,2,4,8,16,24,32,48,64]
 
 
 for thread in threads:
     for alg in  algorithm:
-        ycsb_command = ["java", "-Xmx20g", "-Xms15g", "-cp", "target/myhashdb-1.0.3.jar:../thrift-0.9.2/lib/java/build/libthrift-0.9.2.jar",  \
+        ycsb_command = ["java", "-Xmx20g", "-Xms15g", "-cp", "target/myhashdb-1.0.3.jar:/local/cj.gomes/thrift-0.9.2/lib/java/build/libthrift-0.9.2.jar",  \
                         "bench.tpcc.TpccEmbeded", "-w", str(thread), "-c", str(thread), "-t", str(time), "-tp", alg, "-B" ]
         subprocess.call(ycsb_command)
