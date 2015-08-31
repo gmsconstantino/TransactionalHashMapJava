e65bd6e753a4f5db3be32fdaa97ad6a549cfccb5
Constantino Gomes <gmsconstantino@users.noreply.github.com>
2015-09-01 00:33:23 +0100

    Create package NMSI Array snapshot versions

---
+++ src/main/java/fct/thesis/databaseNMSI_Array/ArraySnapshotsImpl.java
@@ -0,0 +1,99 @@
+package fct.thesis.databaseNMSI_Array;
+
+import fct.thesis.structures.MapEntry;
+
+import java.util.*;
+
+/**
+ * Created by Constantino Gomes on 15/07/15.
+ */
+public class ArraySnapshotsImpl<T extends Transaction> implements SnapshotsIface<T> {
+
+    private static final Transaction NULL_TX = new Transaction<Integer,Integer>();
+    {
+        NULL_TX.abort();
+    }
+
+    MapEntry<T,Long> snapshots[];
+
+    public ArraySnapshotsImpl() {
+        snapshots = new MapEntry[MAX_POS];
+        for (int i=0; i < MAX_POS; i++) {
+            snapshots[i] = new MapEntry<>((T) NULL_TX, null);
+        }
+    }
+
+    @Override
+    public Long get(T tid) {
+        if (tid == snapshots[tid.idxThread].f) // Se o que esta no vetor e' a tx actual entao ok
+            return snapshots[tid.idxThread].s;
+        return null; // Senao deve ser algo passado
+    }
+
+    @Override
+    public void put(T t, long v) {
+        snapshots[t.idxThread].f = t;
+        snapshots[t.idxThread].s = v;
+    }
+
+    @Override
+    public void remove(T t) {
+//        if (t.isActive()){
+//            int index = (int) t.thread.getId()%MAX_POS;
+//            snapshots[t.idxThread].f = (T) NULL_TX;
+//            snapshots[t.idxThread].s = null;
+//        }
+    }
+
+    @Override
+    public Iterable<Map.Entry<T, Long>> entrySet() {
+        return () -> new ArrayIterator();
+    }
+
+    @Override
+    public Collection<? extends Transaction> keySet() {
+        ArrayList<T> keyset = new ArrayList<>(MAX_POS);
+        for (int i = 0; i < MAX_POS; i++) {
+            if (snapshots[i].f != NULL_TX && snapshots[i].f.isActive())
+                keyset.add(snapshots[i].f);
+        }
+        return keyset;
+    }
+
+    private class ArrayIterator implements Iterator {
+
+        private int count;    // the number of elements in the collection
+        private int current;  // the current position in the iteration
+
+        public ArrayIterator() {
+            count = snapshots.length;
+            current = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            for (int i = current; i < count; i++,current++) {
+                if (snapshots[i].s!=null)
+                    break;
+            }
+
+            return (current < count);
+        }
+
+        @Override
+        public Object next() {
+            if (! hasNext())
+                throw new NoSuchElementException();
+
+            return snapshots[current++];
+        }
+
+        //-----------------------------------------------------------------
+        //  The remove operation is not supported in this collection.
+        //-----------------------------------------------------------------
+        public void remove() throws UnsupportedOperationException
+        {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
---
+++ src/main/java/fct/thesis/databaseNMSI_Array/ListIndexable.java
@@ -0,0 +1,86 @@
+package fct.thesis.databaseNMSI_Array;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.ListIterator;
+
+/**
+ * Created by gomes on 08/04/15.
+ */
+public class ListIndexable<T> {
+
+    Long actualVersion = -1L;
+    HashMap<Long, Integer> versionsToPositions = new HashMap<Long, Integer>();
+    ArrayList<T> list = new ArrayList<T>(1000);
+
+    public int indexForVersion(long version){
+        assert (actualVersion>= version);
+
+        if (actualVersion==version)
+            return list.size();
+
+        return versionsToPositions.get(version+1);
+    }
+
+    public void putObject(Long version, T object){
+        if (version > actualVersion){
+            versionsToPositions.put(version, list.size());
+            actualVersion = version;
+        }
+        list.add(object);
+    }
+
+    public void putObject(T object){
+        putObject(actualVersion, object);
+    }
+
+    public ListIterator<T> objectLessThan(Long version){
+        return list.listIterator(indexForVersion(version));
+    }
+
+    public static void main(String[] args) {
+
+        ListIndexable<String> hr = new ListIndexable<String>();
+
+        hr.putObject(0L,"a");
+        hr.putObject("b");
+        hr.putObject("c");
+
+        hr.putObject(1L,"d");
+        hr.putObject("e");
+
+        hr.putObject(2L,"f");
+
+        hr.putObject(3L, "g");
+        hr.putObject("h");
+        hr.putObject("l");
+
+        System.out.print("Version 0 indexForVersion=" + hr.indexForVersion(0L) + " :");
+        print(hr.objectLessThan(0L));
+
+        System.out.print("Version 1 indexForVersion="+hr.indexForVersion(1L)+" :");
+        print(hr.objectLessThan(1L));
+
+        System.out.print("Version 2 indexForVersion="+hr.indexForVersion(1L)+" :");
+        print(hr.objectLessThan(2L));
+
+        System.out.print("Version 3 indexForVersion="+hr.indexForVersion(3L)+" :");
+        print(hr.objectLessThan(3L));
+
+        /* Console:
+            Version 0 indexForVersion=3 :c, b, a,
+            Version 1 indexForVersion=5 :e, d, c, b, a,
+            Version 2 indexForVersion=5 :f, e, d, c, b, a,
+            Version 3 indexForVersion=9 :l, h, g, f, e, d, c, b, a,
+         */
+
+    }
+
+    public static void print(ListIterator<String> it){
+        while (it.hasPrevious()){
+            System.out.print(it.previous() + ", ");
+        }
+        System.out.println();
+    }
+
+}
